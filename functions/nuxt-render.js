de9c4fb6d4bbf364be7e605cbcaecf40c6513213
Sébastien Chopin <seb@nuxtjs.com>
2020-01-16 02:04:42 +0100

    try to render nuxt

--- functions/nuxt-render.js
+++ functions/nuxt-render.js
@@ -1,3 +1,20 @@
-exports.handler = (event, context, callback) => {
-  console.log(event, context, callback)
+const { resolve } = require('path')
+const { Nuxt } = require('nuxt-start')
+const { loadNuxtConfig } = require('@nuxt/cli')
+
+exports.handler = async (event, context) => {
+  // console.log(event, context, callback)
+  const argv = { _: [resolve(__dirname, '..')], 'config-file': 'nuxt.config.js' }
+  const config = await loadNuxtConfig(argv)
+  // Load like nuxt start without starting a server
+  config.dev = false
+  config._start = true
+  // Load nuxt
+  const nuxt = new Nuxt(config)
+  await nuxt.ready()
+
+  // Render a route
+  const { html } = await nuxt.renderRoute('/')
+
+  return { statusCode: 200, body: html }
 }
