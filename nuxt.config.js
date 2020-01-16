0a260cf79f435f150bf88508e09bc1ca90e3420c
Sébastien Chopin <seb@nuxtjs.com>
2020-01-16 01:02:12 +0100

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,22 @@
+export default {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Nuxtlify',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt and Netlify' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ]
+}
